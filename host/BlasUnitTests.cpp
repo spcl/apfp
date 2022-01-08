f9c7c3c3097c41714bbc80d697772381e331359e
Chris Pattison <chpattison@gmail.com>
2022-01-08 02:19:24 +0100

    GEMM unit tests

--- host/BlasUnitTests.cpp
+++ host/BlasUnitTests.cpp
@@ -52,7 +52,8 @@ bool IsClose(apfp::interface::ConstPtr a, apfp::interface::ConstPtr b) {
     auto exp = mpfr_get_exp(ratio.get());
 #endif
     // Require the numbers to match to the first 90% decimal places
-    return exp < -((kMantissaBits * 3 * 9) / 10);
+    // Also demand double precision downcast exactly equal
+    return (exp < -((kMantissaBits * 3 * 9) / 10)) && (mpfr_get_d(a, kRoundingMode) == mpfr_get_d(b, kRoundingMode));
 }
 
 TEST_CASE("Init_Teardown") {
@@ -159,9 +160,91 @@ TEST_CASE("GEMM") {
 
     auto rng = RandomNumberGenerator();
 
-    unsigned long N = GENERATE(0, 1, 2, 8, 15, 16, 31, 32, 33);
     unsigned long M = GENERATE(0, 1, 2, 8, 15, 16, 31, 32, 33);
+    unsigned long N = GENERATE(0, 1, 2, 8, 15, 16, 31, 32, 33);
     unsigned long K = GENERATE(0, 1, 2, 8, 15, 16, 31, 32, 33);
 
+    {
+        CAPTURE(M, N, K);
+        std::vector<apfp::interface::Wrapper> a_matrix;
+        a_matrix.resize(M * K);
+        for (auto& v : a_matrix) {
+            rng.Generate(v.get());
+        }
 
+        std::vector<apfp::interface::Wrapper> b_matrix;
+        b_matrix.resize(K * N);
+        for (auto& v : b_matrix) {
+            rng.Generate(v.get());
+        }
+
+        std::vector<apfp::interface::Wrapper> c_matrix;
+        c_matrix.resize(M * N);
+        for (auto& v : a_matrix) {
+            rng.Generate(v.get());
+        }
+
+        std::vector<apfp::interface::Wrapper> ref_result;
+        ref_result.resize(M * N);
+
+        // Capture inputs for when we explode
+        std::vector<double> a_matrix_d, c_matrix_d;
+        a_matrix_d.resize(a_matrix.size());
+        std::transform(a_matrix.begin(), a_matrix.end(), a_matrix_d.begin(),
+                       [](const auto& v) { return mpfr_get_d(v.get(), kRoundingMode); });
+        c_matrix_d.resize(c_matrix.size());
+        std::transform(c_matrix.begin(), c_matrix.end(), c_matrix_d.begin(),
+                       [](const auto& v) { return mpfr_get_d(v.get(), kRoundingMode); });
+        CAPTURE(a_matrix_d, c_matrix_d);
+
+        // Compute reference result
+        apfp::interface::Wrapper prod_temp;
+        for (unsigned long j = 0; j < N; ++j) {
+            // lower half
+            for (unsigned long i = 0; i < M; ++i) {
+                auto r_idx = i + j * M;
+                apfp::interface::Set(ref_result.at(r_idx).get(), c_matrix.at(r_idx).get());
+
+                for (unsigned long k = 0; k < K; ++k) {
+                    // (AB)_ij = sum_k A(i,k)B(k,j)
+                    apfp::interface::Mul(prod_temp.get(), a_matrix.at(i + k * M).get(), b_matrix.at(k + j * K).get());
+                    apfp::interface::Add(ref_result.at(r_idx).get(), prod_temp.get(), ref_result.at(r_idx).get());
+                }
+            }
+        }
+
+        // Use APFP BLAS library
+        auto error_code = apfp::Gemm(
+            apfp::BlasTrans::normal, apfp::BlasTrans::normal, M, N, K,
+            [&](unsigned long i) { return a_matrix.at(i).get(); }, M,
+            [&](unsigned long i) { return b_matrix.at(i).get(); }, K,
+            [&](unsigned long i) { return c_matrix.at(i).get(); }, M);
+        REQUIRE(!error_code);
+
+        std::vector<double> c_matrix_result_d, c_matrix_ref_result_d;
+        c_matrix_result_d.resize(c_matrix.size());
+        c_matrix_ref_result_d.resize(c_matrix.size());
+        std::transform(c_matrix.begin(), c_matrix.end(), c_matrix_result_d.begin(),
+                       [](const auto& v) { return mpfr_get_d(v.get(), kRoundingMode); });
+        std::transform(ref_result.begin(), ref_result.end(), c_matrix_ref_result_d.begin(),
+                       [](const auto& v) { return mpfr_get_d(v.get(), kRoundingMode); });
+
+        CAPTURE(c_matrix_result_d, c_matrix_ref_result_d);
+
+        // Check all entries are sufficiently close
+        apfp::interface::Wrapper diff;
+        for (unsigned long j = 0; j < N; ++j) {
+            // upper half
+            for (unsigned long i = 0; i < M; ++i) {
+                auto ref_value = ref_result.at(i + j * M).get();
+                auto test_value = ref_result.at(i + j * M).get();
+                CAPTURE(i, j);
+                CAPTURE(PackedFloat(ref_value), PackedFloat(test_value));
+                CAPTURE(mpfr_get_d(ref_value, kRoundingMode), mpfr_get_d(test_value, kRoundingMode));
+                REQUIRE(IsClose(ref_value, test_value));
+            }
+        }
+    }
+
+    ApfpTeardown();
 }

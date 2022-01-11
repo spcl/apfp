894720c610bb3bc18aab7e5cacb4698fa57cb041
Johannes de Fine Licht <definelicht@inf.ethz.ch>
2022-01-11 12:52:53 +0100

    Partition matrix into submatrices to utilize multiple compute units

--- host/TestProgram.cpp
+++ host/TestProgram.cpp
@@ -27,10 +27,12 @@ bool RunTestSimulation(int size_n, int size_k, int size_m, bool verify) {
 #else
 bool RunTest(std::string const &kernel_path, int size_n, int size_k, int size_m, bool verify) {
 #endif
+
     hlslib::ocl::Context context;
     std::cout << "Configuring the device..." << std::flush;
     auto program = context.MakeProgram(kernel_path);
     std::cout << " Done.\n";
+
     // Initialize some random data
     std::cout << "Initializing input data..." << std::flush;
     std::vector<MpfrWrapper> a_mpfr, b_mpfr, c_mpfr;
@@ -65,6 +67,22 @@ bool RunTest(std::string const &kernel_path, int size_n, int size_k, int size_m,
         c_host.emplace_back(x);
     }
     std::cout << " Done.\n";
+
+    // Compute partitions
+    int n_begin[kComputeUnits];
+    int n_end[kComputeUnits];
+    int n_partition_size[kComputeUnits];
+    unsigned long expected_cycles = 0;
+    for (int i = 0; i < kComputeUnits; ++i) {
+        n_begin[i] = (i * size_n) / kComputeUnits;
+        n_end[i] = ((i + 1) * size_n) / kComputeUnits;
+        n_partition_size[i] = n_end[i] - n_begin[i];
+        expected_cycles =
+            std::max(expected_cycles,
+                     (unsigned long)(hlslib::CeilDivide(n_partition_size[i], kTileSizeN) *
+                                     hlslib::CeilDivide(size_m, kTileSizeM) * kTileSizeN * kTileSizeM * size_k));
+    }
+
     // Allocate device memory, padding each buffer to the tile size
     std::cout << "Copying data to the device..." << std::flush;
     constexpr int kDramMapping[] = {1, 0, 2, 3};
@@ -72,35 +90,40 @@ bool RunTest(std::string const &kernel_path, int size_n, int size_k, int size_m,
     std::vector<hlslib::ocl::Buffer<DramLine, hlslib::ocl::Access::read>> b_device;
     std::vector<hlslib::ocl::Buffer<DramLine, hlslib::ocl::Access::readWrite>> c_device;
     for (int i = 0; i < kComputeUnits; ++i) {
-        a_device.emplace_back(context, hlslib::ocl::StorageType::DDR, kDramMapping[i],
-                              kLinesPerNumber * (hlslib::CeilDivide(size_n, kTileSizeN) * kTileSizeN) * size_k);
+        a_device.emplace_back(
+            context, hlslib::ocl::StorageType::DDR, kDramMapping[i],
+            kLinesPerNumber * (hlslib::CeilDivide(n_partition_size[i], kTileSizeN) * kTileSizeN) * size_k);
         b_device.emplace_back(context, hlslib::ocl::StorageType::DDR, kDramMapping[i],
                               kLinesPerNumber * size_k * (hlslib::CeilDivide(size_m, kTileSizeM) * kTileSizeM));
         c_device.emplace_back(context, hlslib::ocl::StorageType::DDR, kDramMapping[i],
-                              kLinesPerNumber * (hlslib::CeilDivide(size_n, kTileSizeN) * kTileSizeN) *
+                              kLinesPerNumber * (hlslib::CeilDivide(n_partition_size[i], kTileSizeN) * kTileSizeN) *
                                   (hlslib::CeilDivide(size_m, kTileSizeM) * kTileSizeM));
         // Copy data to the accelerator cast to 512-bit DRAM lines
-        a_device[i].CopyFromHost(0, kLinesPerNumber * size_n * size_k, reinterpret_cast<DramLine const *>(&a_host[0]));
+        a_device[i].CopyFromHost(0, kLinesPerNumber * n_partition_size[i] * size_k,
+                                 reinterpret_cast<DramLine const *>(&a_host[n_begin[i] * size_k]));
         b_device[i].CopyFromHost(0, kLinesPerNumber * size_k * size_m, reinterpret_cast<DramLine const *>(&b_host[0]));
-        c_device[i].CopyFromHost(0, kLinesPerNumber * size_n * size_m, reinterpret_cast<DramLine const *>(&c_host[0]));
+        c_device[i].CopyFromHost(0, kLinesPerNumber * n_partition_size[i] * size_m,
+                                 reinterpret_cast<DramLine const *>(&c_host[n_begin[i] * size_m]));
     }
     std::cout << " Done.\n";
+
     // In simulation mode, this will call the function "MatrixMultiplication" and run it in software.
     // Otherwise, the provided path to a kernel binary will be loaded and executed.
     std::vector<hlslib::ocl::Kernel> kernels;
     for (int i = 0; i < kComputeUnits; ++i) {
         kernels.emplace_back(program.MakeKernel(MatrixMultiplication, "MatrixMultiplication", a_device[i], b_device[i],
-                                                c_device[i], c_device[i], size_n, size_k, size_m));
+                                                c_device[i], c_device[i], n_partition_size[i], size_k, size_m));
     }
-    const unsigned long expected_cycles = hlslib::CeilDivide(size_n, kTileSizeN) *
-                                          hlslib::CeilDivide(size_m, kTileSizeM) * kTileSizeN * kTileSizeM * size_k;
+
     const float expected_runtime = expected_cycles / 0.3e9;
     std::cout << "The expected number of cycles to completion is " << expected_cycles << ", which is "
               << expected_runtime << " seconds at 300 MHz.\n";
     const auto communication_volume = hlslib::CeilDivide(size_n, kTileSizeN) * hlslib::CeilDivide(size_m, kTileSizeM) *
                                       ((kTileSizeN + kTileSizeM) * size_k + 2 * kTileSizeN * kTileSizeM);
+    const auto bandwidth = 1e-9 * kBytes * communication_volume / expected_runtime;
     std::cout << "This communicates " << 1e-6 * kBytes * communication_volume << " MB, requiring a bandwidth of "
-              << 1e-9 * kBytes * communication_volume / expected_runtime << " GB/s.\n";
+              << bandwidth << " GB/s.\n";
+
     std::cout << "Executing kernel...\n";
     std::vector<hlslib::ocl::Event> events;
     auto start = std::chrono::high_resolution_clock::now();
@@ -118,9 +141,10 @@ bool RunTest(std::string const &kernel_path, int size_n, int size_k, int size_m,
 
     // Copy back result
     std::cout << "Copying back result..." << std::flush;
-    std::vector<std::vector<PackedFloat>> results(kComputeUnits, std::vector<PackedFloat>(size_n * size_m));
+    std::vector<PackedFloat> result(size_n * size_m);
     for (int i = 0; i < kComputeUnits; ++i) {
-        c_device[i].CopyToHost(0, kLinesPerNumber * size_n * size_m, reinterpret_cast<DramLine *>(&results[i][0]));
+        c_device[i].CopyToHost(0, kLinesPerNumber * n_partition_size[i] * size_m,
+                               reinterpret_cast<DramLine *>(&result[n_begin[i] * size_m]));
     }
     std::cout << "Done.\n";
 
@@ -136,21 +160,13 @@ bool RunTest(std::string const &kernel_path, int size_n, int size_k, int size_m,
     std::cout << "Ran in " << elapsed_reference << " seconds.\n";
 
     // Verify results
-    for (int i = 0; i < kComputeUnits; ++i) {
-        for (int n = 0; n < size_n; ++n) {
-            for (int m = 0; m < size_m; ++m) {
-                const PackedFloat res = results[i][n * size_m + m];
-                const PackedFloat ref(c_mpfr[n * size_m + m]);
-                if (ref != res) {
-                    if (kComputeUnits <= 1) {
-                        std::cerr << "Verification failed at (" << n << ", " << m << "):\n\t" << res << "\n\t" << ref
-                                  << "\n";
-                    } else {
-                        std::cerr << "Verification failed for compute unit " << i << " at (" << n << ", " << m
-                                  << "):\n\t" << res << "\n\t" << ref << "\n";
-                    }
-                    return false;
-                }
+    for (int n = 0; n < size_n; ++n) {
+        for (int m = 0; m < size_m; ++m) {
+            const PackedFloat res = result[n * size_m + m];
+            const PackedFloat ref(c_mpfr[n * size_m + m]);
+            if (ref != res) {
+                std::cerr << "Verification failed at (" << n << ", " << m << "):\n\t" << res << "\n\t" << ref << "\n";
+                return false;
             }
         }
     }
